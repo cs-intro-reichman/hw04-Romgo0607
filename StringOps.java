604cef289a0b46deac928e1676303b79169d7a28
Romi Golan <romi.golan@post.runi.ac.il>
2024-01-17 23:15:04 +0200

    1str

--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,22 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String str = "Hello World";
+        System.out.println(capVowelsLowRest(str));
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest (String string) { //recives a string and: capital letters to small, {a,e,i,o,u} to capital, small stays small
+        String str = "";
+        for(int i = 0; i < string.length(); i++) {
+            if((string.charAt(i) <= 'Z') && (string.charAt(i) >= 'A')) {
+                str += (char)(string.charAt(i) + 32);
+            } else if((string.charAt(i) == 'a') || (string.charAt(i) == 'e') || (string.charAt(i) == 'i') || (string.charAt(i) == 'o') || (string.charAt(i) == 'u')) {
+                str += (char)(string.charAt(i) - 32);
+            } else {
+                str += (char)(string.charAt(i));
+            }
+        }
+        return str;
     }
 
     public static String camelCase (String string) {
